b6438dc2882489b25a437c9d48d4983ae82a6d36
parkdusang <pesang72@hanmail.net>
2016-11-28 10:52:34 +0900

    채널부분 완성

---
+++ collaboration/src/GameroomUI.java
@@ -0,0 +1,160 @@
+
+import java.awt.BorderLayout;
+import java.awt.Graphics;
+
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.border.BevelBorder;
+
+
+
+public class GameroomUI {
+
+	private JPanel background_panel, title_panel, drawing_panel, chat_panel;
+	private JPanel user1_panel, user2_panel, user3_panel, user4_panel;
+	private JFrame frame;
+	private JLabel title;
+	private ImageIcon bg = null, d=null, r=null, w=null,e=null,a=null,u=null;
+	static int num;
+	public static int count = 0;
+
+	public GameroomUI() {
+
+		// 전체 프레임 등록
+		frame = new JFrame();
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setLayout(null);
+
+		// 배경 패널 생성
+		bg = new ImageIcon("img/whale.jpg");
+		background_panel = new JPanel() {
+			@Override
+			public void paintComponent(Graphics g) {
+				g.drawImage(bg.getImage(), 0, 0, 1000, 800, null);
+			}
+		};
+
+		title = new JLabel("Drawer Druwa");
+		d = new ImageIcon("img/d.png");
+		r = new ImageIcon("img/r.png");
+		a = new ImageIcon("img/a.png");
+		w = new ImageIcon("img/w.png");
+		e = new ImageIcon("img/e.png");
+		u = new ImageIcon("img/u.png");
+
+		title_panel = new JPanel(){
+			@Override
+			public void paintComponent(Graphics g){
+				//title_panel.add(title);
+				g.drawImage(d.getImage(),120, 20, 20, 30, null);
+				g.drawImage(r.getImage(),145, 20, 20, 30, null);
+				g.drawImage(a.getImage(), 160, 20, 20, 30, null);
+				g.drawImage(w.getImage(), 185, 20, 20, 30, null);
+				g.drawImage(e.getImage(), 200, 20, 20, 30, null);
+				//drawer druwa 이쁜 글씨로 띄우기
+			}
+		};
+
+		chat_panel = new JPanel(){
+			@Override
+			public void paintComponent(Graphics g){
+
+			}
+		};
+
+		drawing_panel = new JPanel(){
+			@Override
+			public void paintComponent(Graphics g){
+
+			}
+		};
+
+
+		user1_panel = new JPanel(){
+			@Override
+			public void paintComponent(Graphics g){
+
+			}
+		};
+
+		user2_panel = new JPanel(){
+			@Override
+			public void paintComponent (Graphics g){
+
+			}
+		};
+
+		user3_panel = new JPanel(){
+			@Override
+			public void paintComponent(Graphics g){
+
+			}
+		};
+
+		user4_panel = new JPanel(){
+			@Override
+			public void paintComponent(Graphics g){
+
+			}
+		};
+		//두께
+		title_panel.setBorder(new BevelBorder(BevelBorder.RAISED));
+		title_panel.setLayout(new BorderLayout());
+		user1_panel.setBorder(new BevelBorder(BevelBorder.RAISED));
+		user1_panel.setLayout(new BorderLayout());
+		user2_panel.setBorder(new BevelBorder(BevelBorder.RAISED));
+		user2_panel.setLayout(new BorderLayout());
+		user3_panel.setBorder(new BevelBorder(BevelBorder.RAISED));
+		user3_panel.setLayout(new BorderLayout());
+		user4_panel.setBorder(new BevelBorder(BevelBorder.RAISED));
+		user4_panel.setLayout(new BorderLayout());
+
+		drawing_panel.setBorder(new BevelBorder(BevelBorder.RAISED));
+		drawing_panel.setLayout(new BorderLayout());
+
+		chat_panel.setBorder(new BevelBorder(BevelBorder.RAISED));
+		chat_panel.setLayout(new BorderLayout());
+
+
+		// 패널들의 위치 조정
+		title_panel.setBounds(0,0,1000, 80);
+		title_panel.setVisible(true);
+		user1_panel.setBounds(50, 100, 200, 200);
+		user1_panel.setVisible(true);
+		user2_panel.setBounds(50, 320, 200,200);
+		user2_panel.setVisible(true);
+		user3_panel.setBounds(750, 100, 200, 200);
+		user3_panel.setVisible(true);
+		user4_panel.setBounds(750, 320, 200, 200);
+
+		drawing_panel.setBounds(300, 80, 400, 500);
+		drawing_panel.setVisible(true);
+		chat_panel.setBounds(300, 500, 400, 220);
+		chat_panel.setVisible(true);
+
+		background_panel.setBounds(0, 0, 1000, 800);
+		background_panel.setVisible(true);
+
+
+		frame.add(chat_panel);
+		frame.add(drawing_panel);
+		frame.add(user1_panel);
+		frame.add(user2_panel);
+		frame.add(user3_panel);
+		frame.add(user4_panel);
+		frame.add(title_panel);
+		frame.add(background_panel);
+		frame.setSize(1000, 800);
+		frame.setVisible(true);
+
+	}
+
+	public static void main(String[] args) {
+
+		GameroomUI gameroom = new GameroomUI();
+
+	}
+
+}
---
+++ collaboration/src/RoomClass.java
@@ -0,0 +1,163 @@
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Toolkit;
+import java.net.Socket;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+
+public class RoomClass extends JPanel {
+	Toolkit tk = Toolkit.getDefaultToolkit();
+	JScrollPane scrollPane;
+	ImageIcon icon, icon2, userIn, userOut;
+	JPanel centerPanel;
+
+	public RoomClass() {
+		icon = new ImageIcon("background.png");
+		this.setSize(290, 125);
+		this.setLayout(null);
+
+		JButton button = new JButton("버튼");
+		this.setLayout(null);
+		this.add(button);
+
+	}
+	public RoomClass(Socket d){
+
+	}
+	public RoomClass(int userNumber, String roomName) {
+		icon = new ImageIcon("background.png");
+		icon2 = new ImageIcon("panelimg.png");
+		this.setLayout(null);
+		this.setSize(290, 125);
+
+		JLabel title = new SetLabelImg("panelimg.png");
+		JLabel userLine = new SetLabelImg("panelimg.png");
+
+
+		title.setText("  " + roomName);
+		title.setBounds(5, 5, 100, 30);
+		this.add(title);
+
+		//userLine.setBounds(5, 45, 200, 40);
+		//this.add(userLine);
+		setPanel(userNumber);
+
+		this.setVisible(true);
+	}
+	public void setPanel(int userNumber){
+		JLabel user1,user2,user3,user4;
+
+
+		centerPanel = new JPanel(){
+			@Override
+			public void paintComponent(Graphics g) {
+				Dimension d = getSize();
+				g.drawImage(icon2.getImage(), 0, 0, d.width, d.height, null);
+				setOpaque(false); //그림을 표시하게 설정,투명하게 조절
+				super.paintComponent(g);
+			}
+		};
+
+		centerPanel.setLayout(null);
+		centerPanel.setBounds(5, 45, 200, 40);
+		if(userNumber == 0){
+			user1 = new SetLabelImg("user2.png");
+			user2 =	new SetLabelImg("user2.png");
+			user3 = new SetLabelImg("user2.png");
+			user4 = new SetLabelImg("user2.png");
+
+			user1.setBounds(10,5, 30,30);
+			centerPanel.add(user1);
+			user2.setBounds(50,5, 30,30);
+			centerPanel.add(user2);
+			user3.setBounds(90,5, 30,30);
+			centerPanel.add(user3);
+			user4.setBounds(130,5, 30,30);
+			centerPanel.add(user4);
+		}
+		if (userNumber == 1) {
+			user1 = new SetLabelImg("user1.png");
+			user2 =	new SetLabelImg("user2.png");
+			user3 = new SetLabelImg("user2.png");
+			user4 = new SetLabelImg("user2.png");
+
+			user1.setBounds(10,5, 30,30);
+			centerPanel.add(user1);
+			user2.setBounds(50,5, 30,30);
+			centerPanel.add(user2);
+			user3.setBounds(90,5, 30,30);
+			centerPanel.add(user3);
+			user4.setBounds(130,5, 30,30);
+			centerPanel.add(user4);
+		}
+		if (userNumber == 2) {
+			user1 = new SetLabelImg("user1.png");
+			user2 =	new SetLabelImg("user1.png");
+			user3 = new SetLabelImg("user2.png");
+			user4 = new SetLabelImg("user2.png");
+
+			user1.setBounds(10,5, 30,30);
+			centerPanel.add(user1);
+			user2.setBounds(50,5, 30,30);
+			centerPanel.add(user2);
+			user3.setBounds(90,5, 30,30);
+			centerPanel.add(user3);
+			user4.setBounds(130,5, 30,30);
+			centerPanel.add(user4);
+		}
+		if (userNumber == 3) {
+			user1 = new SetLabelImg("user1.png");
+			user2 =	new SetLabelImg("user1.png");
+			user3 = new SetLabelImg("user1.png");
+			user4 = new SetLabelImg("user2.png");
+			user1.setBounds(10,5, 30,30);
+			centerPanel.add(user1);
+			user2.setBounds(50,5, 30,30);
+			centerPanel.add(user2);
+			user3.setBounds(90,5, 30,30);
+			centerPanel.add(user3);
+			user4.setBounds(130,5, 30,30);
+			centerPanel.add(user4);
+		}
+		if (userNumber == 4) {
+			user1 = new SetLabelImg("user1.png");
+			user2 =	new SetLabelImg("user1.png");
+			user3 = new SetLabelImg("user1.png");
+			user4 = new SetLabelImg("user1.png");
+			user1.setBounds(10,5, 30,30);
+			centerPanel.add(user1);
+			user2.setBounds(50,5, 30,30);
+			centerPanel.add(user2);
+			user3.setBounds(90,5, 30,30);
+			centerPanel.add(user3);
+			user4.setBounds(130,5, 30,30);
+			centerPanel.add(user4);
+		}
+
+		//centerPanel.setBounds(5, 45, 200, 40);
+		this.add(centerPanel);
+
+	}
+	@Override
+	protected void paintComponent(Graphics g) {
+		// g.fillRect(0, 0, 290, 125);
+		// g.drawImage(img, 0, 0,290, 125, this);
+		// setOpaque(false);
+
+		// Approach 1: Dispaly image at at full size
+		// g.drawImage(icon.getImage(), 0, 0, null);
+		// Approach 2: Scale image to size of component
+		Dimension d = getSize();
+		g.drawImage(icon.getImage(), 0, 0, d.width, d.height, null);
+		// Approach 3: Fix the image position in the scroll pane
+		// Point p = scrollPane.getViewport().getViewPosition();
+		// g.drawImage(icon.getImage(), p.x, p.y, null);
+		setOpaque(false); // 그림을 표시하게 설정,투명하게 조절
+		super.paintComponent(g);
+
+	}
+}
